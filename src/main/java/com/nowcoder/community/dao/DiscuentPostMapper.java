0b0710d296222b2de85791253246cae23a139146
tktlau <laukintung322@gmail.com>
2021-12-02 11:48:42 +0800

    add comment

--- src/main/java/com/nowcoder/community/dao/DiscuentPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscuentPostMapper.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface DiscuentPostMapper {
 
     List<DiscussPost> selectDiscussPost(int userId, int offset, int limit);
-
+    //@param use when only one parameter
     int selectDiscussPostRows(@Param("userId") int userId);
 
 
