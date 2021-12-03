dd2f2025b9acd7053a17d3b62b6c2595e782a7e8
tktlau <laukintung322@gmail.com>
2021-12-03 16:05:21 +0800

    addd session and cookie

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -3,23 +3,32 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.Service.UserService;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
 @Controller
-public class LoginController {
+public class LoginController implements CommunityConstant {
 
     @Autowired
     private UserService userService;
 
+
+
     @RequestMapping(path="/register", method = RequestMethod.GET)
     public String getRegisterPage(){
         return "/site/register";
+    }
+
+    @RequestMapping(path="/login", method = RequestMethod.GET)
+    public String getLoginPage(){
+        return "/site/login";
     }
 
     @RequestMapping(path="/register", method = RequestMethod.POST)
@@ -36,5 +45,23 @@ public class LoginController {
             return "/site/register";
         }
     }
+
+    @RequestMapping(path="/activation/{userId}/{code}", method = RequestMethod.GET)
+    public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code){
+        int result = userService.activation(userId, code);
+        if(result == ACTIVATION_SUCCESS){
+            model.addAttribute("msg","Activation Successfully, you can use your account");
+            model.addAttribute("target","/login");
+        }else if(result == ACTIVATION_REPEAT){
+            model.addAttribute("msg","Account had activated");
+            model.addAttribute("target","/index");
+        }else{
+            model.addAttribute("msg","Activation Failed. Your Activation Code is not correct");
+            model.addAttribute("target","/index");
+        }
+
+        return "/site/operate-result";
+    }
+
 
 }
---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,17 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+
+    int ACTIVATION_SUCCESS=0;
+
+    /**
+     *  Repeat activation
+     */
+
+    int ACTIVATION_REPEAT=01;
+
+    /**
+     * Activation fail
+     */
+    int ACTIVATION_FAILURE=02;
+}
