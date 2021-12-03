6ae434114b0ae099596d39a70daee87c3797fd7e
tktlau <laukintung322@gmail.com>
2021-12-03 12:22:39 +0800

    add Login Function

--- src/main/java/com/nowcoder/community/Service/UserService.java
+++ src/main/java/com/nowcoder/community/Service/UserService.java
@@ -2,8 +2,19 @@ package com.nowcoder.community.Service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
 public class UserService {
@@ -11,8 +22,80 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+
     public User findUserById(int id){
         return userMapper.selectById(id);
+    }
+
+    public Map<String, Object> register(User user) throws IllegalAccessException {
+        Map<String, Object> map = new HashMap<>();
+
+        if(user==null){
+            throw new IllegalAccessException("parameter cannot be null");
+        }
+
+        if(StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMsg","username cannot be null");
+            return map;
+        }
+
+        if(StringUtils.isBlank(user.getPassword())){
+            map.put("passwordMsg","password cannot be null");
+            return map;
+        }
+
+        if(StringUtils.isBlank(user.getEmail())){
+            map.put("EmailMsg","Email cannot be null");
+            return map;
+        }
+
+        //check if user exist
+
+        User u = userMapper.selectByName(user.getUsername());
+        if(u != null){
+            map.put("usernameMsg","User already exists");
+            return map;
+        }
+
+        u = userMapper.selectByEmail(user.getEmail());
+        if(u!=null){
+            map.put("EmailMsg", "Email already exists");
+            return map;
+        }
+
+        // Register user
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5(user.getPassword())+user.getSalt());
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head.%dt.png",new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+
+        userMapper.insertuser(user);
+
+        //Activation email
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+
+        String url = domain+contextPath+"/activation/"+user.getId()+"/"+user.getActivationCode();
+        context.setVariable("url",url);
+        String content = templateEngine.process("/mail/activation", context);
+
+        mailClient.sendMail(user.getEmail(), "Activation Account", content);
+        return map;
     }
 
 }
